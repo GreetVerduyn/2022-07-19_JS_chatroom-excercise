2fc6e49f937399dc63655d339b88b36171ae5888
Greet Verduyn <greetverduyn@proximus.be>
2022-07-19 17:21:08 +0200

    clients can sent message

--- client/client.js
+++ client/client.js
@@ -1,4 +1,41 @@
-"use strict";
 
 let socket = io.connect();
 
+let textClient = document.getElementById('input')
+
+
+document.getElementById('sentToAll').onclick = function () {
+   socket.emit("sendToAll", textClient.value);
+}
+
+document.getElementById('sent').onclick = function () {
+   socket.emit("sndMsg", textClient.value);
+}
+
+socket.on('displayMessage', (message) => {
+   target.innerHTML += '<br>'+message;
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+// send a message to the server
+socket.emit("hello from client", 5, "6", { 7: Uint8Array.from([8]) });
+
+// receive a message from the server
+socket.on("hello from server", (...args) => {
+    // ...
+});*/
--- server/server.js
+++ server/server.js
@@ -24,3 +24,31 @@ io.on('connection', (socket) => {
 
 });
 
+/*
+socket.on('sendToAll', (message) =>{
+    io.emit("displayMessage", (message));
+});
+*/
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+io.on("connection", (socket) => {
+    // send a message to the client
+    socket.emit("hello from server", 1, "2", { 3: Buffer.from([4]) });
+
+    // receive a message from the client
+    socket.on("hello from client", (...args) => {
+        // ...
+    });
+});
+*/
